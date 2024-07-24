51d0283af401ece6a6bc051b72c253604d7387cf
lfriesen <linden.friesen@protonmail.com>
2024-07-24 12:06:07 -0500

    test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0427090fd1714168b" # us-east-1
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
